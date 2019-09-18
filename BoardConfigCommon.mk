690e6954b34da3d049abad0c701b0517aa499a63
srfarias <erikofarias@gmail.com>
2019-09-18 23:18:04 +0200

    Whyred : TWOLIP -> Whyred
    Change-Id: Ica6c87d562f257e7395fab7a80dfc8f4e5b8553c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,7 +63,9 @@ BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm660
+ifeq ($(BOARD_KERNEL_COMMON),true)
+TARGET_KERNEL_SOURCE := kernel/xiaomi/whyred
+endif
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # QCOM hardware
